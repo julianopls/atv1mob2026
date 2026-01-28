9076d688f79551838e6cfe33eab31a64d4d34f4b
julianopls <brolezzijuliano@gmail.com>
2026-01-28 16:08:01 -0300

    Novo

---
+++ script.js
@@ -0,0 +1,9 @@
+function obterDados (e){
+     fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+     .then(resp => resp.json())
+    .then(resp =>{
+        console.log(resp);
+        e.innerHTML = resp.USDBRL.bid
+    })
+    
+}
